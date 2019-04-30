d9bdfbb57fd86e42f3030a97444dbaaa23fba53f
Piotr Szymaszek <piotr.szymaszek@fsi.pl>
2019-04-30 13:44:49 +0200

    Load extensions configuration via compiler pass

--- DataGridBundle.php
+++ DataGridBundle.php
@@ -7,19 +7,25 @@
  * file that was distributed with this source code.
  */
 
+declare(strict_types=1);
+
 namespace FSi\Bundle\DataGridBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use FSi\Bundle\DataGridBundle\DependencyInjection\Compiler\DataGridPass;
+use FSi\Bundle\DataGridBundle\DependencyInjection\Compiler\ExtensionsPass;
 use FSi\Bundle\DataGridBundle\DependencyInjection\Compiler\TemplatePathPass;
 use FSi\Bundle\DataGridBundle\DependencyInjection\FSIDataGridExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class DataGridBundle extends Bundle
 {
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
+        // this needs to be first, otherwise the extensions will not be loaded in
+        // DataGridPass
+        $container->addCompilerPass(new ExtensionsPass());
         $container->addCompilerPass(new DataGridPass());
         $container->addCompilerPass(new TemplatePathPass());
     }
---
+++ DependencyInjection/Compiler/ExtensionsPass.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * (c) FSi sp. z o.o. <info@fsi.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace FSi\Bundle\DataGridBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
+final class ExtensionsPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../../Resources/config'));
+        if (true === $container->hasExtension('doctrine')) {
+            $loader->load('datagrid_doctrine.xml');
+        }
+
+        if (true === $container->hasExtension('stof_doctrine_extensions')) {
+            $loader->load('datagrid_gedmo.xml');
+        }
+    }
+}
